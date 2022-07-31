a8c4adbf9837f4c9ece1f22f1e34c15b09d36a58
MarioEduardo <eduardoat44@gmail.com>
2022-07-31 13:46:24 -0500

    Creación de archivos y codigos base

---
+++ Chunk.js
@@ -0,0 +1,9 @@
+function chunk(array, size) {
+    // Code goes here
+  };
+  
+  const data = [1, 2, 3, 4, 5, 6, 7, 8]
+  
+  console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
+  console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
+  console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
---
+++ DeepEqual.js
@@ -0,0 +1,14 @@
+function deepEqual(a, b) {
+    // Code goes here
+   }
+   
+   const john = {
+    firstName: 'John',
+    lastName: 'Doe'
+   }
+   
+   console.log('Test 1:', deepEqual(1, 1)) // true
+   console.log('Test 2:', deepEqual(1, '1')) // false
+   console.log('Test 3:', deepEqual(john, john)) // true
+   console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
+   console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
