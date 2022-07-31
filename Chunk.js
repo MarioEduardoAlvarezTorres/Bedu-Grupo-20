a592f6f915dd9d92561707c5c8144ffafbb67ef3
MarioEduardo <eduardoat44@gmail.com>
2022-07-31 17:08:37 -0500

    Programa chunk terminado

--- Chunk.js
+++ Chunk.js
@@ -1,5 +1,17 @@
 function chunk(array, size) {
-    // Code goes here
+    arrayFinal = []                                 //ARRAYFINAL = RESULTADO QUE SE MOSTRARÁ
+    arrayAux = []                                   //ARRAYAUX = SIRVE PARA IR ALMACENANDO LOS SUBARREGLOS
+    cont=0;                                         //CONT = CONTADOR PARA VERIFICAR EL TAMAÑO DEL SUBARREGLO
+    for (let i = 0; i < array.length; i++) {
+        arrayAux.push(array[i]);                    //SE AGREGA EL CONTENIDO DEL ARREGLO ORIGINAL AL AUXILIAR
+        cont++;
+        if(cont === size || i+1 === array.length){  //EN CASO DE FORMAR EL SUBARREGLO (DE TAMAÑO N) O NO TENER MAS ELEMENTOS DENTRO DEL ARREGLO ORIGINAL
+            arrayFinal.push(arrayAux)               //AGREGAMOS EL ARREGLO AUXILIAR AL FINAL
+            cont=0                                  //SE REINICIA EL CONTADOR
+            arrayAux = []                           //SE "VACIA" EL ARREGLO AUXILIAR
+        }
+    }
+    return arrayFinal
   };
   
   const data = [1, 2, 3, 4, 5, 6, 7, 8]
